77995ca8d961dd395762ef956b3f6f948af12fed
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2022-04-20 18:58:42 -0300

    change style file name

---
+++ src/Components/Styles/Carousel.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+
+export const StyledCarousel = styled.div`
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+export const StyledCard = styled.div`
+    img {
+        width: 85%;
+    }
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 220px;
+        height: 220px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+`;
+

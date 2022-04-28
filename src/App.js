740f5819dac0205bb11042b697b28433eee3fbda
Exequiel Olea <90924371+Exequiel86@users.noreply.github.com>
2022-04-28 19:47:37 -0300

    Creacion del Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import { FooterContainer } from "./containers/footer";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <FooterContainer />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { GlobalStyles } from './global-styles';
 import App from './App';
 
 
+
 ReactDOM.render(
-  <React.StrictMode>
+    <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    </>
+    , document.getElementById('root'));
+

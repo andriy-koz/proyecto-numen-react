261b768545dd7417605864bd1ece9d0dd2044d97
Gonzalo Núñez <92396308+Gonzalonu99@users.noreply.github.com>
2022-05-09 21:29:19 -0300

    d in styleD file

---
+++ src/styles/StyledWines.js
@@ -0,0 +1,19 @@
+import styledComponents from "styled-components";
+
+
+export const StyledWinesContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    justify-items: center;
+    align-items: center;
+`;
+
+export const StyledCards = styled.div`
+    width: 150px;
+    height: 150px;
+    border: 4px solid black;
+    margin: 25%;
+    padding: 25%;
+    background-color: antiquewhite;
+`;

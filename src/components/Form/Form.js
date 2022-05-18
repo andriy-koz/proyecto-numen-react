38b4524d5f7e22061d1c59f2df193a6ac291e06e
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-05-18 14:20:34 -0300

    validaciones

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,6 +8,27 @@ import {
 import Input from './Input';
 
 const Form = () => {
+  let nameHasError = true;
+  let lastNameHasError = true;
+  let emailHasError = true;
+
+  const nameHasErrorHandler = hasError => {
+    nameHasError = hasError;
+  };
+  const lastNameHasErrorHandler = hasError => {
+    lastNameHasError = hasError;
+  };
+  const emailHasErrorHandler = hasError => {
+    emailHasError = hasError;
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+    if (!nameHasError && !lastNameHasError && !emailHasError) {
+      console.log('Submitted');
+    } else return;
+  };
+
   return (
     <StyledForm>
       <FormContainer>
@@ -15,12 +36,27 @@ const Form = () => {
           <h3>Unite a nuestra Lista VIP</h3>
           <p>Recibe novedades, ofertas e invitaciones exclusivas!</p>
         </LayoutDiv>
-        <form>
+        <form onSubmit={submitHandler}>
           <InputLayoutDiv>
-            <Input label='Nombre' id='name' type='text' />
-            <Input label='Apellido' id='lastName' type='text' />
+            <Input
+              label='Nombre'
+              id='name'
+              type='text'
+              hasError={nameHasErrorHandler}
+            />
+            <Input
+              label='Apellido'
+              id='lastName'
+              type='text'
+              hasError={lastNameHasErrorHandler}
+            />
           </InputLayoutDiv>
-          <Input label='Email' id='email' type='email' />
+          <Input
+            label='Email'
+            id='email'
+            type='email'
+            hasError={emailHasErrorHandler}
+          />
           <button>ENVIAR</button>
         </form>
       </FormContainer>
--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,10 +1,32 @@
 import { StyledInput } from './Input.styled';
+import React, { useState } from 'react';
 
 const Input = props => {
+  const [value, setValue] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const valueIsValid = value !== '';
+
+  const onChangeHandler = e => {
+    setValue(e.target.value);
+    setHasError(false);
+  };
+  const onBlurHandler = () => {
+    setHasError(!valueIsValid);
+  };
+
+  props.hasError(hasError);
+
   return (
     <StyledInput>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} id={props.id} />
+      <input
+        type={props.type}
+        id={props.id}
+        value={value}
+        onChange={onChangeHandler}
+        onBlur={onBlurHandler}
+      />
+      {hasError && <p>Invalid</p>}
     </StyledInput>
   );
 };

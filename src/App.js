df4e9ec1e692313f96c01d198f759d517e262fb8
GiovannaBernabei <giovanna.bernabei@outlook.es>
2022-05-31 20:21:12 -0300

    styles.js
    Co-authored-by: Gonzalo Núñez <Gonzalonu99@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./styles.css";
-import styled from "styled-components"
+
+
 
 function App() {
-  const Container = styled.div`
   
-  `;  
-  
- return (
-  <Container>
-
-  </Container>
- );
+  return (
+    <Container/>
+  )
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,5 @@
+export const Container = styled.div`
+background-color: grey;
+text-align: auto;
+border: solid red
+`;

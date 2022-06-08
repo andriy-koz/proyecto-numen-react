b5d4b68b030a4421440ceae5d3b3f8ffc1088a65
GiovannaBernabei <giovanna.bernabei@outlook.es>
2022-06-08 03:00:33 -0300

    completo axios

--- server/axios.js
+++ server/axios.js
@@ -1,1 +1,14 @@
-axios.get('https://sanantoniowinery.com/')
+axios.get('/https://sanantoniowinery.com/', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  }); 
